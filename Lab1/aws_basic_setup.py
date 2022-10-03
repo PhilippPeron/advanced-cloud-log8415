9cef030707a90f28639a1d483a00d17c2d6707c5
Rad-Ali <rad.ali@polymtl.ca>
2022-10-03 15:02:25 -0400

    Implemented different subnets for each instance. Implemented methods to create elastic load balancers and attach a target group to it.

--- Lab1/aws_basic_setup.py
+++ Lab1/aws_basic_setup.py
@@ -1,40 +1,50 @@
+# Sets up a security group. Creates instances, target groups and elastic load balancers.
+
 import boto3
 from botocore.exceptions import ClientError
 
-# Sets up a security group, and creates an instance associated with it.
+# Global variables
 EC2_CLIENT = boto3.client('ec2')
 EC2_RESOURCE = boto3.resource('ec2')
 ELB_CLIENT = boto3.client('elbv2')
+SN_ALL = EC2_CLIENT.describe_subnets() # Obtain a list of all subnets
+N_SUBNETS = len(SN_ALL['Subnets']) #Obtain amount of subnets
+
 
 #Function to create {num_instances} amount of {instance_type} instances
 #Returns a list of instance objects
-def create_instances(
-    num_instances, 
-    instance_type):
-    instances = EC2_RESOURCE.create_instances(
-        ImageId='ami-0149b2da6ceec4bb0', # ubuntu 20.04 x86 (free) (the one we used in the first lab session iirc)
-        MinCount=num_instances,
-        MaxCount=num_instances,
-        InstanceType=instance_type, # we will change accordingly (this one is free)
-        SecurityGroupIds=[security_group_id], # we use the security group id just created above.
-        TagSpecifications=[
-            {
-                'ResourceType': 'instance',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': instance_type +'lab1-ec2-instance'
-                    },
-                ]
-            },
-        ]
-        )
-    print(f'{instances} are starting')
+def create_ec2(num_instances, instance_type): 
+    instances = []
+    for i in range(num_instances):
+        instances.append(EC2_RESOURCE.create_instances(
+            ImageId='ami-0149b2da6ceec4bb0', # ubuntu 20.04 x86 (free) (the one we used in the first lab session iirc)
+            MinCount=1,
+            MaxCount=1,
+            InstanceType=instance_type, # we will change accordingly (t2.nano is free for testing purposes)
+            SecurityGroupIds=[security_group_id], # we use the security group id just created above.
+            SubnetId=SN_ALL['Subnets'][i%N_SUBNETS]['SubnetId'], # we distribute instances evenly across all subnets
+            
+            TagSpecifications=[
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': instance_type +'-lab1-ec2-instance-' +str(i+1)
+                        },
+                    ]
+                },
+            ]
+            )[0])
+        print(f'{instances[i]} is starting')
     return instances
 
 #Function to create a target group called {group_name} that includes {instances}
-#Returns __
-def create_target_group(group_name, vpc_id, instances):
+#Returns the target group's arn
+def create_tg(
+    group_name, 
+    vpc_id, 
+    instances):
     elb_response = ELB_CLIENT.create_target_group(
         Name=group_name,
         Protocol='HTTP',
@@ -54,6 +64,34 @@ def create_target_group(group_name, vpc_id, instances):
                 }
             ]
         )
+    return targ_grp_arn
+
+#Function to create a elastic load balancer called {elb_name} and attach it to target group with arn {tg_arn}
+#Returns ___
+def create_elb(elb_name, tg_arn):
+    subnet_ids = []
+    for i in range(N_SUBNETS):
+        subnet_ids.append(SN_ALL['Subnets'][i]['SubnetId'])
+
+    elb = ELB_CLIENT.create_load_balancer(
+        Name=elb_name,
+        Subnets=subnet_ids,
+        Scheme='internet-facing',
+        Type='application',
+        IpAddressType='ipv4'
+    )
+
+    #Attach ELB to Target Group
+    response = ELB_CLIENT.create_listener(
+    DefaultActions = [{
+        'TargetGroupArn': tg_arn,
+        'Type': 'forward',
+    }, ],
+    LoadBalancerArn = elb['LoadBalancers'][0]['LoadBalancerArn'],
+    Port = 80,
+    Protocol = 'HTTP',
+    )
+    return
 
 # get our virtual cloud id
 vpc_response = EC2_CLIENT.describe_vpcs()
@@ -94,15 +132,22 @@ except ClientError as e:
         print(e)
     except ClientError as e:
         print(e)
+        exit(1)
 
-
-print('CREATING AN INSTANCE ASSOCIATED WITH THE SECURITY GROUP')
-
-num_instances = 1 #number of instances to create
-instance_type = 't2.nano' #change to m4.large
-m4_instances = create_instances(num_instances, instance_type)
+#Create a instances with the created security group rules
+print('CREATING INSTANCES ASSOCIATED WITH THE SECURITY GROUP')
+m4_num_instances = 3 #number of instances to create
+m4_instance_type = 't2.nano' #change to m4.large
+m4_instances = create_ec2(m4_num_instances, m4_instance_type)
 
 #Create a target group for the instances created
 print('CREATING A TARGET GROUP FOR THE M4 INSTANCES AND REGISTERING THEM')
 m4_group_name = 'm4-Group'
-create_target_group(m4_group_name, vpc_id, m4_instances)
+m4_group_arn = create_tg(m4_group_name, vpc_id, m4_instances)
+
+#Create a load balancer for the target group created
+print('CREATING LOAD BALANCER AND ATTACHING IT TO M4 TARGET GROUP')
+m4_elb_name = 'm4-load-balancer'
+create_elb(m4_elb_name,m4_group_arn)
+
+
