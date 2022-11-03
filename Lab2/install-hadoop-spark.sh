4b23ac56cb5479a2f9310428afa3d15a51a83cce
Philipp Peron <philipp.peron@gmail.com>
2022-11-03 14:35:28 -0400

    Change spark version to 2.0.0

--- Lab2/install-hadoop-spark.sh
+++ Lab2/install-hadoop-spark.sh
@@ -10,10 +10,15 @@ echo "export PATH=$JAVA_HOME/bin:$PATH" >> ~/.profile
 echo "export HADOOP_HOME=/usr/local/hadoop-3.3.4" >> ~/.profile
 echo "export PATH=/usr/local/hadoop-3.3.4/bin:$PATH" >> ~/.profile
 echo "export PATH=$HADOOP_HOME/bin:$PATH" >> ~/.profile
+echo "export PATH=/usr/local/spark/bin:$PATH" >> ~/.profile
+echo "export SPARK_HOME=/usr/local/spark" >> ~/.profile
 source ~/.profile
 
 # install spark
-sudo apt install python3-pip -y
-pip install pyspark
+wget https://archive.apache.org/dist/spark/spark-2.0.0/spark-2.0.0-bin-hadoop2.7.tgz
+tar -xzf spark-2.0.0-bin-hadoop2.7.tgz
+mv spark-2.0.0-bin-hadoop2.7 spark
+# sudo apt install python3-pip -y
+# pip install pyspark
 
 
