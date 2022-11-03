742cb91707a48154f8ca49bfa29a7a45522f623d
Philipp Peron <philipp.peron@gmail.com>
2022-11-03 16:23:23 -0400

    Add run loop

--- Lab2/word_counter.sh
+++ Lab2/word_counter.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+
+LOG_FILE='word_count.time'
+
 sudo mkdir text_files
 cd text_files
+# Download files
 wget https://tinyurl.com/4vxdw3pa
 wget https://tinyurl.com/kh9excea
 wget https://tinyurl.com/dybs9bnk
@@ -11,6 +15,28 @@ wget https://tinyurl.com/2h6a75nk
 wget https://tinyurl.com/vwvram8
 wget https://tinyurl.com/weh83uyn
 cd ..
+# Create hdfs folder
 hdfs dfs -mkdir input
-hdfs dfs -copyFromLocal text_files/* input
-time hadoop jar /usr/local/hadoop-3.3.4/share/hadoop/mapreduce/hadoop-map/reduce-examples-3.3.4.jar wordcount input output
+
+# Retrieve wordcount.py path
+WORD_COUNT_PYSPARK=`sudo find / -name wordcount.py`
+# Copy files into hadoop file system and process input files in text_files directory
+for FILE in `ls text_files/*-00-* | cut -d"/" -f2`
+do
+  hdfs dfs -copyFromLocal "text_files/$FILE" input
+  echo  >>LOG_FILE
+  echo "$FILE" >>LOG_FILE
+  echo 'Spark:' >>LOG_FILE
+  # Run spark command
+  { time python3 "$WORD_COUNT_PYSPARK" "text_files/$FILE"; } 2>&1 | tail -n3 >>LOG_FILE
+  echo 'Hadoop:' >>LOG_FILE
+  # Run hadoop command
+  { time hadoop jar /usr/local/hadoop-3.3.4/share/hadoop/mapreduce/hadoop-map/reduce-examples-3.3.4.jar wordcount "input/$FILE" output; } 2>&1 | tail -n3 >>LOG_FILE
+  echo "" >>LOG_FILE
+  # Cleanup output folder
+  hdfs dfs -rm -r output
+done
+
+# Cleanup input folder
+hdfs dfs -rm -r input
+op jar /usr/local/hadoop-3.3.4/share/hadoop/mapreduce/hadoop-map/reduce-examples-3.3.4.jar wordcount input output
