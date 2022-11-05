a34f9ead00c0973ef8cce2a9fe761d0fa2551284
Rad Ali <rad.ali@polymtl.ca>
2022-11-05 14:47:10 -0400

    -Created automation script for log file analysis -Added correct amount of iterations of tests -Cleaned log file creation script

--- Lab2/word_counter.sh
+++ Lab2/word_counter.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#install matplotlib to plot the performance graphs
+pip3 install matplotlib
+
 LOG_FILE='word_count.time'
 
 sudo mkdir text_files
@@ -20,23 +23,30 @@ cd ..
 
 # Retrieve wordcount.py path
 WORD_COUNT_PYSPARK=`sudo find / -name wordcount.py`
-# Copy files into hadoop file system and process input files in text_files directory
-for FILE in `ls text_files/* | cut -d"/" -f2`
+
+# repeat three times to obtain an average
+for i in 1 2 3
 do
-  /usr/local/hadoop-3.3.4/bin/hdfs dfs -mkdir output
-  /usr/local/hadoop-3.3.4/bin/hdfs dfs -copyFromLocal "text_files/$FILE" input
-  echo  >>LOG_FILE
-  echo "$FILE" >>LOG_FILE
-  echo 'Spark:' >>LOG_FILE
-  # Run spark command
-  { time python3 "$WORD_COUNT_PYSPARK" "text_files/$FILE"; } 2>&1 | tail -n3 >>LOG_FILE
-  echo 'Hadoop:' >>LOG_FILE
-  # Run hadoop command
-  { time /usr/local/hadoop-3.3.4/bin/hadoop jar /usr/local/hadoop-3.3.4/share/hadoop/mapreduce/hadoop-map/reduce-examples-3.3.4.jar wordcount "input/$FILE" output; } 2>&1 | tail -n3 >>LOG_FILE
-  echo "" >>LOG_FILE
-  # Cleanup output folder
-  /usr/local/hadoop-3.3.4/bin/hdfs dfs -rm -r output
+# Copy files into hadoop file system and process input files in text_files directory
+  for FILE in `ls text_files/* | cut -d"/" -f2`
+  do
+    /usr/local/hadoop-3.3.4/bin/hdfs dfs -mkdir output
+    /usr/local/hadoop-3.3.4/bin/hdfs dfs -copyFromLocal "text_files/$FILE" input
+    echo "$FILE" >>LOG_FILE
+    echo "Spark" >>LOG_FILE
+    # Run spark command
+    { time python3 "$WORD_COUNT_PYSPARK" "text_files/$FILE"; } 2>&1 | tail -n3 | head -n1 | sed -e 's/\<real\>//g' | tr  -d '[:blank:]'>>LOG_FILE
+    echo "Hadoop" >>LOG_FILE
+    # Run hadoop command
+    { time /usr/local/hadoop-3.3.4/bin/hadoop jar /usr/local/hadoop-3.3.4/share/hadoop/mapreduce/hadoop-map/reduce-examples-3.3.4.jar wordcount "input/$FILE" output; } 2>&1 | tail -n3 | head -n1 | sed -e 's/\<real\>//g' | tr  -d '[:blank:]'>>LOG_FILE
+    # Cleanup output folder
+    /usr/local/hadoop-3.3.4/bin/hdfs dfs -rm -r output
+  done
 done
 
-# Cleanup input folder
+# Cleanup input and text files folders
 /usr/local/hadoop-3.3.4/bin/hdfs dfs -rm -r input
+sudo rm -r text_files
+
+#Start the log file processing script
+python3 wordcount_graphs.py
---
+++ Lab2/wordcount_graphs.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+
+file = open('LOG_FILE', 'r')
+lines = file.readlines()
+n_lines = len(lines)
+n_tests = int(n_lines/5)
+
+datasets =[]
+spark_times=[]
+hadoop_times=[]
+instances = 0
+#Loop through all wordcount results 
+for i in range(n_tests):
+    #format the data that will be used to create graphs
+    key = lines[i*5].replace("\n","")
+    spark_minutes = lines[i*5+2].replace("\n","").split("m")
+    spark_minutes[1]=spark_minutes[1].replace("s","")
+    spark_seconds = float(spark_minutes[0])*60+float(spark_minutes[1])
+
+    hadoop_minutes = lines[i*5+4].replace("\n","").split("m")
+    hadoop_minutes[1]=hadoop_minutes[1].replace("s","")
+    hadoop_seconds = float(hadoop_minutes[0])*60+float(hadoop_minutes[1])
+    if key in datasets:
+        instances += 1
+        spark_times[i%9]+=spark_seconds
+        hadoop_times[i%9]+=hadoop_seconds
+    else:
+        datasets.append(key)
+        spark_times.append(spark_seconds)
+        hadoop_times.append(hadoop_seconds)
+      
+file.close()
+instances = 1 + instances/9 #obtain the amaount of times the datasets were processed by spark and hadoop
+
+#Obtain average times
+spark_res = []
+for val in spark_times:
+    spark_res.append(val/instances)
+hadoop_res = []
+for val in hadoop_times:
+    hadoop_res.append(val/instances)
+#Create performance graph
+plt.plot(datasets,spark_res,hadoop_res)
+plt.title('Performance Comparison of Hadoop and Spark')
+plt.ylabel('Real Seconds Average')
+plt.xlabel('Datasets')
+plt.xticks(rotation=90)
+plt.legend(['Spark times','Hadoop times'])
+#Output graph to an image file
+plt.savefig('HadoopVsSpark.png')
